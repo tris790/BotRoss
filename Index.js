6a360f9358ec3a27aa9dcca05ffa51909d193f06
Tristan Deschamps <tris790@gmail.com>
2016-11-13 00:24:30 -0500

    Removed unnecessary

--- Index.js
+++ Index.js
@@ -6,8 +6,6 @@ const CommandHandler = require("./commands/commandhandler");
 const FFmpeg = require("ffmpeg");
 const Eris = require("eris");
 const FS = require("fs");
-var youtubedl = require('youtube-dl');
-
 
 var bot = new Eris.CommandClient(Auth.discordtoken, {}, {
     description: Config.Description,
