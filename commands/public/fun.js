39ed037872d120de77eae9587c6ceeef4a474dda
Tristan Deschamps <tris790@gmail.com>
2016-11-18 19:33:12 -0500

    Fixed typo "reponse" to "response"

--- commands/public/fun.js
+++ commands/public/fun.js
@@ -1,15 +1,16 @@
 "use strict";
 
-function Install(bot){
-  var ask = bot.registerCommand("ask", (msg, args) => {
-    var reponse = ["Yes", "No", "Maybe", "I don't care!"];
-    var rand = Math.floor(Math.random() * (reponse.length - 0)) + 0;
-    return response[rand];
-  },{
-    description: "Asks Bot Ross a question.",
-    fullDescription: "The bot will answer your question.",
-    usage: "<question>"
-  });
+function Install(bot) {
+    var ask = bot.registerCommand("ask", (msg, args) => {
+        var response = ["Yes", "No", "Maybe", "I don't care!"];
+        console.log(response);
+        var rand = Math.floor(Math.random() * (response.length - 0)) + 0;
+        return response[rand];
+    }, {
+        description: "Asks Bot Ross a question.",
+        fullDescription: "The bot will answer your question.",
+        usage: "<question>"
+    });
 }
 
 module.exports.Install = Install;
